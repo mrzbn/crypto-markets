417c20c03008961a3d84f6781716a8221003af63
Mohammad Mahdi Marzbani <mohammadmahdi.m@rahgoshagroup.com>
2024-07-19 16:00:31 +0330

    add price formatting function to price_model.dart

--- lib/shared/price_model.dart
+++ lib/shared/price_model.dart
@@ -1,11 +1,24 @@
 import 'package:decimal/decimal.dart';
+import 'package:decimal/intl.dart';
+import 'package:intl/intl.dart';
 
 class PriceModel {
-
   final Decimal price;
   final int decimalDigitsCount;
   final String unit;
 
-  PriceModel({required this.price, required this.decimalDigitsCount, required this.unit});
+  String formatWithCommas() {
+    final formatter = DecimalFormatter(
+      NumberFormat.decimalPatternDigits(
+        locale: "en_us",
+        decimalDigits: decimalDigitsCount,
+      ),
+    );
+    return formatter.format(price);
+  }
 
+  PriceModel(
+      {required this.price,
+      required this.decimalDigitsCount,
+      required this.unit});
 }
