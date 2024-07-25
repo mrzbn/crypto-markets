c1f2077a3a36059690a4446c8369b677e832e5b4
Mohammad Mahdi Marzbani <mohammadmahdi.m@rahgoshagroup.com>
2024-07-25 23:52:10 +0330

    display markets list

--- lib/markets/markets_screen.dart
+++ lib/markets/markets_screen.dart
@@ -1,4 +1,8 @@
+import 'package:crypto_markets/markets/market_list_item_ui_model.dart';
+import 'package:crypto_markets/markets/market_list_item_widget.dart';
+import 'package:crypto_markets/markets/market_list_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MarketsScreen extends StatefulWidget {
   const MarketsScreen({super.key});
@@ -10,6 +14,27 @@ class MarketsScreen extends StatefulWidget {
 class _MarketsScreenState extends State<MarketsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("markets screen");
+    return Consumer(builder: (c, ref, child) {
+      AsyncValue<List<MarketListItemUiModel>> marketsList =
+          ref.watch(marketsListProvider);
+      return Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+          body: Center(
+            child: switch (marketsList) {
+              AsyncData(:final value) => ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (c, index) {
+                    return MarketListItemWidget(model: value[index]);
+                  },
+                ),
+              AsyncError(:final error, :final stackTrace) =>
+                Text("${error.toString()}\n${stackTrace.toString()}"),
+              _ => const CircularProgressIndicator(),
+            },
+          ),
+        ),
+      );
+    });
   }
 }
