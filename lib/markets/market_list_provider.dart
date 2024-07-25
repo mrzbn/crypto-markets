69c8447ec4fe8a91f960225eb2403c1fb321842b
Mohammad Mahdi Marzbani <mohammadmahdi.m@rahgoshagroup.com>
2024-07-25 23:51:54 +0330

    create market_list_provider.dart

---
+++ lib/markets/market_list_provider.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:crypto_markets/markets/market_list_item_ui_model.dart';
+import 'package:crypto_markets/markets/market_list_item_ui_model_mapper.dart';
+import 'package:crypto_markets/shared/currency_api_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part "market_list_provider.g.dart";
+
+@riverpod
+Future<List<MarketListItemUiModel>> marketsList(MarketsListRef ref) async {
+  final url = Uri.https("api-web.tabdeal.org", "/r/plots/currency_prices/");
+  final response = await http.get(url);
+  final jsonList = jsonDecode(response.body) as List;
+  final mapper = MarketListItemUiModelMapper();
+  return jsonList
+      .map((e) => CurrencyApiModel.fromJson(e))
+      .map((e) => mapper.call(e))
+      .toList();
+}
---
+++ lib/markets/market_list_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'market_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$marketsListHash() => r'5b8b933cd53054e962d611ba574364c8abd3c7d6';
+
+/// See also [marketsList].
+@ProviderFor(marketsList)
+final marketsListProvider =
+    AutoDisposeFutureProvider<List<MarketListItemUiModel>>.internal(
+  marketsList,
+  name: r'marketsListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$marketsListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef MarketsListRef
+    = AutoDisposeFutureProviderRef<List<MarketListItemUiModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
