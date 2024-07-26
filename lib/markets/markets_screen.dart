41803d2bea2af93f6efdbe87b74e529a9601144d
Mohammad Mahdi Marzbani <mohammadmahdi.m@rahgoshagroup.com>
2024-07-26 14:23:34 +0330

    use ConsumerStatefulWidget instead of Consumer

--- lib/markets/markets_screen.dart
+++ lib/markets/markets_screen.dart
@@ -4,26 +4,25 @@ import 'package:crypto_markets/markets/market_list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MarketsScreen extends StatefulWidget {
+class MarketsScreen extends ConsumerStatefulWidget {
   const MarketsScreen({super.key});
 
   @override
-  State<MarketsScreen> createState() => _MarketsScreenState();
+  ConsumerState<MarketsScreen> createState() => _MarketsScreenState();
 }
 
-class _MarketsScreenState extends State<MarketsScreen> {
+class _MarketsScreenState extends ConsumerState<MarketsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (c, ref, child) {
-      AsyncValue<List<MarketListItemUiModel>> marketsList =
-          ref.watch(marketsListProvider);
-      return Directionality(
-        textDirection: TextDirection.rtl,
-        child: Scaffold(
-          body: Center(
-            child: switch (marketsList) {
-              AsyncData(:final value) => ListView.separated(
-                  separatorBuilder: (BuildContext context, int index) {
+    AsyncValue<List<MarketListItemUiModel>> marketsList =
+        ref.watch(marketsListProvider);
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        body: Center(
+          child: switch (marketsList) {
+            AsyncData(:final value) => ListView.separated(
+                separatorBuilder: (BuildContext context, int index) {
                     return const SizedBox(height: 20);
                   },
                   itemCount: value.length,
@@ -38,6 +37,5 @@ class _MarketsScreenState extends State<MarketsScreen> {
           ),
         ),
       );
-    });
   }
 }
